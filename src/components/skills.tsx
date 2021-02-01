0bf3063a8893ccf85b18dc9b3c99c7133cd0c995
konradstanski <kstanisz@ualberta.ca>
2021-02-01 02:47:37 -0700

    remove icons

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,45 +1,9 @@
-import Lightning from "./svgs/lightning";
-import Jamstack from "./svgs/jamstack";
-import Wifi from "./svgs/wifi";
-import Lighthouse from "./svgs/lighthouse";
-import Plus from "./svgs/plus";
-import Notion from "./svgs/notion";
-import Edit from "./svgs/edit";
-import Scroll from "./svgs/scroll";
-import Test from "./svgs/react";
+import ReactIcon from "./svgs/react";
 
 const skills = [
   {
     text: "React",
-    icon: Test
-  },
-  {
-    text: "JAMstack based",
-    icon: Jamstack
-  },
-  {
-    text: "Always available",
-    icon: Wifi
-  },
-  {
-    text: "Customizable",
-    icon: Edit
-  },
-  {
-    text: "Incremental SSG",
-    icon: Plus
-  },
-  {
-    text: "MIT Licensed",
-    icon: Scroll
-  },
-  {
-    text: "Edit via Notion",
-    icon: Notion
-  },
-  {
-    text: "Great scores",
-    icon: Lighthouse
+    icon: ReactIcon
   }
 ];
 
