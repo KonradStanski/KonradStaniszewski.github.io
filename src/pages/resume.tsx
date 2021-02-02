c62a7d9a60209e1373d360a66b61fcd20c59fd1b
konradstanski <kstanisz@ualberta.ca>
2021-02-01 21:29:23 -0700

    hide pdf toolbar

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -10,13 +10,13 @@ export default () => (
     <Header titlePre="Projects" />
     <div className={sharedStyles.layout}>
       <IframeResizer
-        src="/resume.pdf"
+        src="/resume.pdf#toolbar=0"
         style={{
           display: "block",
           margin: "auto",
           overflow: "hidden",
           width: "33em",
-          height: "49em"
+          height: "47em"
         }}
       />
     </div>
