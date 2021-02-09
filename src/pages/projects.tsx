99f816028e6ea8ea703319ab2e5a6f870f92a964
konradstanski <kstanisz@ualberta.ca>
2021-02-09 00:53:29 -0700

    modifying projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -7,9 +7,12 @@ export default () => (
   <>
     <Header titlePre="Projects" />
     <div className={sharedStyles.layout}>
-      <h1>CAUTION</h1>
-      <h1>UNDERGOING CONSTRUCTION</h1>
-      <h1>CAUTION</h1>
+      <h2>
+        <ExtLink href="https://github.com/konradStanski">
+          Please check out my projects on GitHub while I work on adding them to
+          this page!
+        </ExtLink>
+      </h2>
     </div>
   </>
 );
