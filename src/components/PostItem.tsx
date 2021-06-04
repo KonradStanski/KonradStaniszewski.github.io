0e020c9f17c4ee85d9b8993f65509acd8e5b3a73
konradStanski <kstanisz@ualberta.ca>
2021-06-04 13:52:17 -0600

    Change PostItem hover and layout

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -10,30 +10,28 @@ type postProps = {
 
 const PostItem = (props: postProps): JSX.Element => {
     return (
-        <article key={props.post.slug} className="mt-12">
-            <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-                {format(parseISO(props.post.date), 'MMMM dd, yyyy')}
-            </p>
-            <h1 className="mb-2 text-xl">
-                <Link
-                    as={`/${props.type}/${props.post.slug}`}
-                    href={`/${props.type}/[slug]`}
-                >
+        <Link
+            as={`/${props.type}/${props.post.slug}`}
+            href={`/${props.type}/[slug]`}
+        >
+            <div
+                key={props.post.slug}
+                className="mt-8 cursor-pointer hover:shadow-md hover:bg-gray-50 dark:hover:bg-opacity-5 hover:bg-opacity-40 rounded py-6 px-4"
+            >
+                <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                    {format(parseISO(props.post.date), 'MMMM dd, yyyy')}
+                </p>
+                <h1 className="mb-2 text-xl">
                     <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
                         {props.post.title}
                     </a>
-                </Link>
-            </h1>
-            <p className="mb-3">{props.post.description}</p>
-            <p>
-                <Link
-                    as={`/${props.type}/${props.post.slug}`}
-                    href={`/${props.type}/[slug]`}
-                >
-                    <a>Read More</a>
-                </Link>
-            </p>
-        </article>
+                </h1>
+                <p className="mb-3">{props.post.description}</p>
+                <p className="mb-0">
+                    <a>Read More...</a>
+                </p>
+            </div>
+        </Link>
     );
 };
 
