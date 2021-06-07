003ab45932cb9b1e4ce0d4389380d2320727f18d
konradStanski <kstanisz@ualberta.ca>
2021-06-07 09:34:50 -0600

    Add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import React from 'react';
 import '../styles/globals.css';
+import * as gtag from '../utils/gtag';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url: URL) => {
+            gtag.pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
     return (
         <ThemeProvider
             attribute="class"
@@ -15,4 +30,4 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     );
 };
 
-export default MyApp;
+export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,28 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../utils/gtag';
 
 class MyDocument extends Document {
     render(): JSX.Element {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                        // eslint-disable-next-line react/no-danger
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GA_TRACKING_ID}', {
+                            page_path: window.location.pathname,
+                            });`,
+                        }}
+                    />
+                </Head>
                 <body className="global-layout-style">
                     <Main />
                     <NextScript />
