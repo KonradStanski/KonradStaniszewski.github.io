740d8d42cbbe1ca1c62448fc502d4b73d2358e6f
konradStanski <kstanisz@ualberta.ca>
2021-06-14 14:03:03 -0600

    Animate hamburger Menu

--- src/components/Header/Hamburger.tsx
+++ src/components/Header/Hamburger.tsx
@@ -5,36 +5,40 @@ type props = {
     navOpen: boolean;
 };
 
-const Hamburger = (props: props): JSX.Element => (
-    <button
-        aria-label="hamburger menu"
-        type="button"
-        style={{ height: '42px', width: '42px' }}
-        onClick={props.clickHamburger}
-        className="block items-center md:hidden"
-    >
-        <svg
-            className="h-6 w-6 fill-current m-auto hover:opacity-100"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            style={{
-                opacity: '0.5',
-                transition: 'opacity 0.3s ease',
-            }}
+const Hamburger = (props: props): JSX.Element => {
+    const genericHamburgerLine = `h-[2px] w-[24px] my-[3px] rounded-full
+    bg-black dark:bg-white
+    transition ease transform duration-300`;
+
+    return (
+        <button
+            aria-label="hamburger menu"
+            type="button"
+            onClick={props.clickHamburger}
+            className="md:hidden flex flex-col h-[48px] w-[48px] justify-center items-center group"
         >
-            {props.navOpen ? (
-                <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                />
-            ) : (
-                <path
-                    fillRule="evenodd"
-                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                />
-            )}
-        </svg>
-    </button>
-);
+            <div
+                className={`${genericHamburgerLine} ${
+                    props.navOpen
+                        ? 'rotate-45 translate-y-2 opacity-50 group-hover:opacity-100'
+                        : 'opacity-50 group-hover:opacity-100'
+                }`}
+            />
+            <div
+                className={`${genericHamburgerLine} ${
+                    props.navOpen
+                        ? 'opacity-0'
+                        : 'opacity-50 group-hover:opacity-100'
+                }`}
+            />
+            <div
+                className={`${genericHamburgerLine} ${
+                    props.navOpen
+                        ? '-rotate-45 -translate-y-2 opacity-50 group-hover:opacity-100'
+                        : 'opacity-50 group-hover:opacity-100'
+                }`}
+            />
+        </button>
+    );
+};
 export default Hamburger;
