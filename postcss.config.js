6585e933fe70312e3f4f6a02695c71a203ad57d7
konradStanski <kstanisz@ualberta.ca>
2021-06-21 14:57:40 -0600

    Fix jit and add lockfile

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
     plugins: {
-        // tailwindcss: {},
-        '@tailwindcss/jit': {},
+        tailwindcss: {},
+        // '@tailwindcss/jit': {},
         autoprefixer: {},
     },
 };
