d8c68957176764dfd5c960a4c41bb578fe28a667
konradStanski <kstanisz@ualberta.ca>
2021-07-26 12:54:25 -0600

    Fix images (again)

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@ module.exports = {
     distDir: 'build',
     images: {
         domains: [
-            'images.unsplash.com',
-            's3.us-west-2.amazonaws.com'
+            'images.unsplash.com'
         ],
     },
 };
