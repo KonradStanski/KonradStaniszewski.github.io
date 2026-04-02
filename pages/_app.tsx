16b5f31c91d96f8a21f46e26e219b062e92daca7
Konrad Staniszewski <konrad.a.staniszewski@gmail.com>
2026-04-02 10:17:44 -0700

    Remove Google Analytics and @next/third-parties package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-import { GoogleAnalytics } from "@next/third-parties/google";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const fullWidth = (Component as any).fullWidth;
@@ -18,7 +17,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       </div>
       <div className={fullWidth ? "px-4" : "max-w-5xl mx-auto px-4"}>
         <Component {...pageProps} />
-        <GoogleAnalytics gaId="G-1M9XX68M0R"/>
       </div>
       <div className="flex flex-col max-w-5xl mx-auto px-4">
         <Footer />
